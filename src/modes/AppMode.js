c41a851ff1eb072f4a1b287ad393c85dd2a637f0
Dark Hole <dark.hole1@yandex.ru>
2023-03-11 16:22:35 +0300

    Add drawing target

--- src/modes/AppMode.js
+++ src/modes/AppMode.js
@@ -10,11 +10,24 @@ export class AppMode {
         }
     }
 
-    onRender() {
-        this._canvasCtx.drawText('Paused');
+    onRender(viewport) {
+        // Drawing properly centered text is hard T-T
+        this._canvasCtx.drawText('Paused', 100, 100);
     }
 
     accept() {
         // TODO
+    }
+
+    drawTarget(pos, viewport) {
+        const ctx = this._canvasCtx;
+        const x = pos[0] * viewport.width;
+        const y = pos[1] * viewport.height;
+        
+        ctx.save();
+        ctx.beginPath(x, y, 100, 100, 0, Math.PI * 2);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.restore();
     }
 }
