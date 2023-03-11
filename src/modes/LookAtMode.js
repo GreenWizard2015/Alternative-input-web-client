5858ba53392961706ab1c6bbb6bebf0bc5d4b8ac
Dark Hole <dark.hole1@yandex.ru>
2023-03-11 16:17:39 +0300

    Add lookat mode

---
+++ src/modes/LookAtMode.js
@@ -0,0 +1,46 @@
+import { AppMode } from "./AppMode"
+
+export class LookAtMode extends AppMode {
+    constructor({ canvasCtx }) {
+        super({ canvasCtx });
+        // Maybe constant is better?
+        this._visibleT = 5.0;
+    }
+
+    _next() {
+        this._pos = [Math.random(), Math.random()];
+        this._active = false;
+        this._startT = null;
+    }
+
+    onKeyDown(event) {
+        super.onKeyDown(event);
+        // Notice: Numpad keys won't work. If this's unwanted behaviour
+        // use event.key instead.
+        if(event.code = 'ArrowRight') {
+            this._active = true;
+        }
+    }
+
+    onRender(viewport) {
+        super.onRender(viewport);
+
+        // onRender = on_tick + on_render
+        if(this._active) {
+            const dT = Date.now() - this._startT;
+            if(this._visibleT < dT) {
+                this._next();
+            }
+        } else {
+            this._startT = Date.now();
+        }
+
+        this.drawTarget(this._pos, viewport);
+    }
+
+    accept() {
+        if(this._active) {
+            super.accept();
+        }
+    }
+}
