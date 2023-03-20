d535ed4d9b4350e585dbc4b4485ba6ca24a27c6b
Dark Hole <dark.hole1@yandex.ru>
2023-03-20 19:25:38 +0300

    Add place variable and ui

--- src/components/UI.js
+++ src/components/UI.js
@@ -4,6 +4,7 @@ import UIHelp from './UIHelp';
 import UIStart from './UIStart';
 
 const validateUser = ({ name, uuid }) => name.length > 0 && uuid.length > 0
+const validatePlace = validateUser
 
 export default function UI({
   onWebcamChange, goFullscreen, onStart,
@@ -14,6 +15,7 @@ export default function UI({
   // TODO: Implement place creation. User can only create new places, not select existing ones. Use localStorage to store place id and their name. Use UUID library to generate place id. Literally just button "New place" that creates new place id and name it "dd.mm.yyyy hh:mm:ss". If place id is null, create new place id and name it "dd.mm.yyyy hh:mm:ss". If place id is not matched to any place, create new place id and name it "dd.mm.yyyy hh:mm:ss". In any case, just create new place id and name it "dd.mm.yyyy hh:mm:ss".
   const [subMenu, setSubMenu] = React.useState('');
   const [user, setUser] = useLocalStorageState('user', { name: '', uuid: '' })
+  const [place, setPlace] = useLocalStorageState('place', { name: '', uuid: '' })
 
   function showHelp() {
     setSubMenu('help')
@@ -34,7 +36,7 @@ export default function UI({
             <WebcamSelector onWebcamChange={onWebcamChange} /> 
           */}
             <p>User: {user.name}<button>{validateUser(user) ? 'Edit' : 'Create'}</button></p>
-            <p>Place:</p>
+            <p>Place: {place.name}<button>{validatePlace(user) ? 'Edit' : 'Create'}</button></p>
             <button onClick={showHelp}>Help</button>
             <button onClick={() => setSubMenu('start')} disabled={!validateUser(user)}>Start</button>
             <button onClick={goFullscreen}>Fullscreen</button>
