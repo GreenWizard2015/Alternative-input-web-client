5f622a44f5b34bff0253fcf661011376c97fb10c
Dark Hole <dark.hole1@yandex.ru>
2023-03-20 19:42:33 +0300

    Add creating user and place

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocalStorageState } from 'utils/hooks';
 import UIHelp from './UIHelp';
 import UIStart from './UIStart';
@@ -16,6 +16,7 @@ export default function UI({
   const [subMenu, setSubMenu] = React.useState('');
   const [user, setUser] = useLocalStorageState('user', { name: '', uuid: '' })
   const [place, setPlace] = useLocalStorageState('place', { name: '', uuid: '' })
+  const [tempName, setTempName] = useState('')
 
   function showHelp() {
     setSubMenu('help')
@@ -28,6 +29,40 @@ export default function UI({
         return <UIHelp onClose={() => setSubMenu('')} />
       case 'start':
         return <UIStart onStart={onStart} />
+      case 'user':
+        return <>
+          <div>
+            <span>Enter user name </span>
+            <input value={tempName} onInput={e => setTempName(e.target.value)} />
+          </div>
+          <div>
+            <button onClick={() => {
+              setUser({ name: tempName, uuid: crypto.randomUUID() })
+              setSubMenu('')
+            }}>Ok</button>
+            <button onClick={() => {
+              setTempName('')
+              setSubMenu('')
+            }}>Cancel</button>
+          </div>
+        </>
+      case 'place':
+        return <>
+          <div>
+            <span>Enter place name </span>
+            <input value={tempName} onInput={e => setTempName(e.target.value)} />
+          </div>
+          <div>
+            <button onClick={() => {
+              setPlace({ name: tempName, uuid: crypto.randomUUID() })
+              setSubMenu('')
+            }}>Ok</button>
+            <button onClick={() => {
+              setTempName('')
+              setSubMenu('')
+            }}>Cancel</button>
+          </div>
+        </>
       default:
         return (
           <>
@@ -35,8 +70,8 @@ export default function UI({
             <p>Webcamera:</p>
             <WebcamSelector onWebcamChange={onWebcamChange} /> 
           */}
-            <p>User: {user.name}<button>{validateUser(user) ? 'Edit' : 'Create'}</button></p>
-            <p>Place: {place.name}<button>{validatePlace(user) ? 'Edit' : 'Create'}</button></p>
+            <p>User: {user.name}<button onClick={() => setSubMenu('user')}>{validateUser(user) ? 'Edit' : 'Create'}</button></p>
+            <p>Place: {place.name}<button onClick={() => setSubMenu('place')}>{validatePlace(user) ? 'Edit' : 'Create'}</button></p>
             <button onClick={showHelp}>Help</button>
             <button onClick={() => setSubMenu('start')} disabled={!validateUser(user) || !validatePlace(place)}>Start</button>
             <button onClick={goFullscreen}>Fullscreen</button>
