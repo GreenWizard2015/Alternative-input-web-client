87c4f996b4fb7d92daca3d8a2d1d3a93529c8d90
GreenWizard2015 <forwork.anton@gmail.com>
2024-05-09 08:13:50 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 // next.config.js
+const path = require('path');
 
 module.exports = {
   distDir: 'build',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'], // Указываете расширения файлов, которые должны рассматриваться как страницы
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve('./src')); // Добавляет src в модульные пути
+    return config;
+  },  
   experimental: {
     serverActions: {
       bodySizeLimit: '100mb',
