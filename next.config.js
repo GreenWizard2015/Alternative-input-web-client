1fbfc1067e17eeddf39411e2700c7087af7e7319
GreenWizard2015 <forwork.anton@gmail.com>
2024-05-09 09:37:36 +0000

    XXXX

--- next.config.js
+++ next.config.js
@@ -2,16 +2,5 @@
 const nextConfig = {};
 module.exports = {
   ...nextConfig,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.txt$/i,
-      use: ['raw-loader'],
-    });
-    config.module.rules.push({
-      test: /\.svg$/i,
-      use: ['raw-loader'],
-    });
-
-    return config;
-  }
+  distDir: 'build',
 };
