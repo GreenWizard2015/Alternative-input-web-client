2ccf06f5563e8cb5a5358b75775932c4ec185564
GreenWizard2015 <forwork.anton@gmail.com>
2024-05-13 20:31:00 +0000

    misc

--- src/modes/onMenuTick.ts
+++ src/modes/onMenuTick.ts
@@ -28,7 +28,34 @@ export function onMenuTick({
     
     canvasCtx.putImageData(leftEyeImage, 0, 0);
     canvasCtx.putImageData(rightEyeImage, leftEyeImage.width, 0);
-    
+
+    // // Define the scale factor
+    // let scaleFactor = 16;
+    // // Create a temporary canvas for the images
+    // let tempCanvas = document.createElement('canvas');
+    // let tempCtx = tempCanvas.getContext('2d');
+    // // Function to draw image data onto the main canvas with scaling
+    // const drawScaledImage = function(imageData, x, y) {
+    //   tempCanvas.width = imageData.width;
+    //   tempCanvas.height = imageData.height;
+    //   tempCtx.putImageData(imageData, 0, 0);
+    //   canvasCtx.drawImage(
+    //     tempCanvas,
+    //     0,
+    //     0,
+    //     tempCanvas.width,
+    //     tempCanvas.height,
+    //     x,
+    //     y,
+    //     tempCanvas.width * scaleFactor,
+    //     tempCanvas.height * scaleFactor
+    //   );
+    // }
+    // // Draw the left eye image onto the main canvas with scaling
+    // drawScaledImage(leftEyeImage, 0, 0);
+    // // Draw the right eye image onto the main canvas with scaling
+    // drawScaledImage(rightEyeImage, leftEyeImage.width * scaleFactor, 0);
+
     // print other info
     canvasCtx.fillStyle = "black";
     canvasCtx.font = "16px Arial";
