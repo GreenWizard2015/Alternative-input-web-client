a9714cd9ee27adf2155891c386df3af66ab17bd6
Green Wizard <forwork.anton@gmail.com>
2025-12-11 17:11:03 +0200

    update serverside

--- serverside/list.php
+++ serverside/list.php
@@ -1,13 +1,11 @@
 <?php
 $directoryPath = "chunks";
 
-$baseUrl = "http://vksf.com.ru.host1617252.serv21.hostland.pro/AI/chunks";
+$baseUrl = "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . "/chunks";
 
 $gzFiles = glob($directoryPath . "/*.gz");
 
-$gzUrls = array_map(function($filePath) use ($baseUrl, $directoryPath) {
-    return $baseUrl . '/' . basename($filePath);
-}, $gzFiles);
+$gzUrls = array_map(fn($filePath) => $baseUrl . '/' . basename($filePath), $gzFiles);
 
 echo json_encode($gzUrls);
 ?>
--- serverside/upload.php
+++ serverside/upload.php
@@ -42,8 +42,8 @@ echo json_encode([ "success" => true, "total" => $total ]);
 
 exit(0);
 
-function get_guid() {
-    $data = PHP_MAJOR_VERSION < 7 ? openssl_random_pseudo_bytes(16) : random_bytes(16);
+function get_guid(): string {
+    $data = random_bytes(16);
     $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // Set version to 0100
     $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // Set bits 6-7 to 10
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
